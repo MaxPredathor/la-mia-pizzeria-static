9f3a13d4dca272ea56dd5e2f81c220b8dee400e1
MaxPredathor <max.gilli98@gmail.com>
2024-05-10 15:14:43 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Immagine { get; set; }
+        public float Prezzo { get; set; }
+
+        public Pizza(string name,  string descrizione, string image, float price)
+        {
+            Nome = name;
+            Descrizione = descrizione;
+            Immagine = image;
+            Prezzo = price;
+        }
+    }
+}
